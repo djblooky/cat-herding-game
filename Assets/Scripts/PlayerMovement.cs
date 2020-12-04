741c26474b312013d3e212c9cc8fd9524d3d2095
Charlie Phillips <hi.charliephillips@gmail.com>
2020-12-04 15:15:04 -0600

    added placeholder player sprites and created a player with a move script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
