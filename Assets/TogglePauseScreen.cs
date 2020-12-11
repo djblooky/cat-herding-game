c17e2075de861fdfdbfb8d81e303afa991cf19b3
Charlie Phillips <hi.charliephillips@gmail.com>
2020-12-11 15:27:20 -0600

    escape to pause functionality, no buttons yet

---
+++ Assets/TogglePauseScreen.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class TogglePauseScreen : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private bool inPauseMenu = false;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.interactable = false;
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!inPauseMenu)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        inPauseMenu = true;
+        Time.timeScale = 0;
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.interactable = true;
+    }
+
+    private void Unpause()
+    {
+        inPauseMenu = false;
+        Time.timeScale = 1;
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.interactable = false;
+    }
+}
