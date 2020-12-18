50ded3c269c4ece80ffb074a514a6832abd5788c
Charlie Phillips <hi.charliephillips@gmail.com>
2020-12-17 22:50:37 -0600

    added player sfx

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,12 @@
 public class Player : MonoBehaviour
 {
     private MoveController moveController;
+    private AudioSource audioSource;
 
     private void Start()
     {
         moveController = GetComponent<MoveController>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -15,10 +17,31 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    private void PlayFootsteps()
+    {
+        audioSource.volume = Random.Range(0.01f, 0.05f);
+        audioSource.pitch = Random.Range(0.8f, 1.1f);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+        
+    }
+
     protected void Move()
     {
-        var xInput = Input.GetAxisRaw("Horizontal");
-        var yInput = Input.GetAxisRaw("Vertical");
+        var xInput = 0f;
+        var yInput = 0f;
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            xInput = Input.GetAxisRaw("Horizontal");
+            yInput = Input.GetAxisRaw("Vertical");
+            PlayFootsteps();
+        }
+        else
+            audioSource.Stop();
+
 
         moveController.SetMoveInput(xInput, yInput);
     }
